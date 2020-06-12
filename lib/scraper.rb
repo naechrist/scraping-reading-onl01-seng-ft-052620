8bf6f6232e13ba7d0faf6ecf93e7846c7f401a62
Jenae Christensen <729jenae.christensen@gmail.com>
2020-06-12 16:55:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,4 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
